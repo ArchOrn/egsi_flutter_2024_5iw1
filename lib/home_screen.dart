44aa75f5e01361c8ae401c31e86a6d05fef2fd26
Raphaël Kiffer <kiffer.raphael@gmail.com>
2024-04-11 10:41:32 +0200

    Demo with StatefulWidget

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,54 +1,31 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Color _color = Colors.red;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        centerTitle: true,
-        leading: const Icon(
-          Icons.airplanemode_on,
-          color: Colors.white,
-        ),
-        title: const Text(
-          'My Title',
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
       backgroundColor: Colors.white,
-      body: const Center(
-        child: Text(
-          'Hello',
-          style: TextStyle(
-            color: Colors.amber,
-            fontSize: 22,
-            fontWeight: FontWeight.bold,
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              _color = _color == Colors.red ? Colors.blue : Colors.red;
+            });
+          },
+          child: Container(
+            width: 80,
+            height: 80,
+            color: _color,
           ),
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'Favorites',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today),
-            label: 'Today',
-          ),
-        ],
-        selectedItemColor: Colors.blue,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.blue,
-        shape: const CircleBorder(),
-        child: const Icon(
-          Icons.add,
-          color: Colors.white,
         ),
       ),
     );
