98bf31db703073cdad2ebca6918cce5c1f250f62
Raphaël Kiffer <kiffer.raphael@gmail.com>
2024-04-11 10:17:05 +0200

    Demo with base Scaffold and base screen components

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,16 +5,50 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+        leading: const Icon(
+          Icons.airplanemode_on,
+          color: Colors.white,
+        ),
+        title: const Text(
+          'My Title',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
       backgroundColor: Colors.white,
-      body: Center(
+      body: const Center(
         child: Text(
           'Hello',
           style: TextStyle(
-            color: Colors.purple,
-            fontSize: 18,
-            fontWeight: FontWeight.w500,
+            color: Colors.amber,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
           ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Today',
+          ),
+        ],
+        selectedItemColor: Colors.blue,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.blue,
+        shape: const CircleBorder(),
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
         ),
       ),
     );
