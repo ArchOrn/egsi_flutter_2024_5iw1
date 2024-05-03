5eebf1f6a87fe102f9305dc0ac2b7a115c1757e5
Raphaël Kiffer <kiffer.raphael@gmail.com>
2024-05-03 16:29:09 +0200

    Demo with Bloc and products fetching

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,13 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_5iw1/home/blocs/home_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.white,
+    return BlocProvider(
+      create: (context) => HomeBloc()..add(HomeDataLoaded()),
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          body: BlocBuilder<HomeBloc, HomeState>(
+            builder: (context, state) {
+              if (state is HomeLoading) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+
+              if (state is HomeDataLoadingError) {
+                return Center(
+                  child: Text(
+                    state.errorMessage,
+                    style: const TextStyle(color: Colors.red),
+                  ),
+                );
+              }
+
+              if (state is HomeDataLoadingSuccess) {
+                return ListView.builder(
+                  itemBuilder: (context, index) {
+                    final product = state.products[index];
+                    return ListTile(
+                      title: Text(product.title),
+                      subtitle: Text(product.description),
+                    );
+                  },
+                  itemCount: state.products.length,
+                );
+              }
+
+              return const SizedBox();
+            },
+          ),
+        ),
       ),
     );
   }
